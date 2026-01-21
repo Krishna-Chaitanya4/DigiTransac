c735959f4e3abd46e84a20693f1d9236b277f49f
Krishna Chaitanya Yadav Perugu <peruguyadav@microsoft.com>
2026-01-22 03:58:49 +0530

    refactor: extract TransactionForm sub-components
    Create reusable components from TransactionForm:
    - TransactionTypeSelector: Debit/Credit/Transfer tabs
    - TagTokenInput: Tag selection with inline create
    - LocationPicker: Location capture and search
    - RecurringSection: Recurring transaction settings
    - SplitCategoriesSection: Split transaction management
    
    Components are ready for gradual integration into main form.
    Build and all 462 web tests pass.

---
+++ web/src/components/transaction-form/index.ts
@@ -0,0 +1,5 @@
+export { TransactionTypeSelector } from './TransactionTypeSelector';
+export { TagTokenInput } from './TagTokenInput';
+export { LocationPicker } from './LocationPicker';
+export { RecurringSection } from './RecurringSection';
+export { SplitCategoriesSection, validateSplits } from './SplitCategoriesSection';
