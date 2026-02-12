337968fc39d34f73d90d0b55015c0bd3c4887a64
Krishna Chaitanya Yadav Perugu <peruguyadav@microsoft.com>
2026-02-12 17:42:58 +0530

    fix: remove unused useCallback import from AccountCard.tsx

--- web/src/components/accounts/AccountCard.tsx
+++ web/src/components/accounts/AccountCard.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 import { useTheme } from '../../context/ThemeContext';
 import { useIsMobile } from '../../hooks/useMediaQuery';
 import { useHaptics } from '../../hooks/useHaptics';
